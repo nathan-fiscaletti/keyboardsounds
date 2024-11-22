ea8ce37b10100c10be77295c23505ad12c8231fd
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2024-11-22 02:02:35 -0600

    continued prototyping editor ui

--- application/src/ui/editor.jsx
+++ application/src/ui/editor.jsx
@@ -17,9 +17,15 @@ import SaveIcon from '@mui/icons-material/Save';
 import SettingsIcon from '@mui/icons-material/Settings';
 import CloseIcon from '@mui/icons-material/Close';
 import LabelIcon from '@mui/icons-material/Label';
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import DeleteOutlineOutlinedIcon  from "@mui/icons-material/DeleteOutlineOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 import Card from "@mui/material/Card";
-import { Typography, Box, Tooltip, IconButton, TextField, List, ListItem, Button, Select, MenuItem, Divider, Dialog, InputAdornment } from "@mui/material";
+import { Typography, Box, Tooltip, IconButton, TextField, List, ListItem, Button, Select, MenuItem, Divider, Dialog, InputAdornment, ListItemText, Chip } from "@mui/material";
 
 import Keyboard from 'react-simple-keyboard';
 import 'react-simple-keyboard/build/css/index.css';
@@ -138,9 +144,68 @@ const keyboardNumPadEndOptions = {
 // Sources
 // Keys
 
+function SourceListItem({ name, press, release }) {
+  const secondaryText = (press && release) ? `${press}, ${release}` : `${press}` || `${release}`;
+
+  const typeVariant = press && release ? "filled" : "outlined";
+  const typeLabel = press && release ? "Press & Release" : "Press Only";
+  const typeDescription = press && release ? "Distinct press and release sounds" : "Single press sound";
+
+  return (
+    <ListItem
+      disableGutters
+      secondaryAction={
+        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+          <Tooltip title={typeDescription} placement="top" arrow>
+            <Chip sx={{ mr: 1 }} size="small" label={typeLabel} variant={typeVariant} color="primary" />
+          </Tooltip>
+
+          <Tooltip title="Listen" placement="top" arrow>
+            <IconButton color="primary" sx={{ mr: 1 }}>
+              <PlayArrowIcon />
+            </IconButton>
+          </Tooltip>
+
+          <Tooltip title="Edit Source" placement="top" arrow>
+            <IconButton color="primary" sx={{ mr: 1 }}>
+              <EditIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      }
+      sx={{
+        borderRadius: 1,
+        mb: 1,
+        bgcolor: "background.default",
+        pl: 2,
+      }}
+    >
+      <ListItemText
+        primary={(
+          <Typography variant="body1">
+            {name}
+          </Typography>
+        )}
+        secondary={secondaryText}
+        secondaryTypographyProps={{
+          noWrap: true,
+          variant: "caption",
+          style: {
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            maxWidth: 'calc(100vw - 275px)',
+          }
+        }}
+      />
+    </ListItem>
+  )
+}
+
 function Editor() {
   const [profileDetailsOpen, setProfileDetailsOpen] = useState(false);
   const [manageSourcesOpen, setManageSourcesOpen] = useState(false);
+  const [addSourceOpen, setAddSourceOpen] = useState(false);
 
   return (
     <ThemeProvider theme={theme}>
@@ -192,13 +257,69 @@ function Editor() {
             mb: 2,
           }}>
             <Typography variant="h6">Manage Sources</Typography>
-            <IconButton onClick={() => setManageSourcesOpen(false)}>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+              <Button variant="outlined" size="small" startIcon={<AddIcon />} sx={{ mr: 1 }} onClick={() => setAddSourceOpen(true)}>
+                Add Source
+              </Button>
+              <IconButton onClick={() => setManageSourcesOpen(false)}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+          </Box>
+          <TextField label="Search" size="small" sx={{ mb: 1 }} InputProps={{
+            endAdornment: <InputAdornment position="end"><SearchIcon /></InputAdornment>,
+          }} />
+          <Box sx={{ 
+            overflow: 'auto',
+            maxHeight: 'calc(100vh - 200px)',
+            scrollbarWidth: 'none',
+            '&::-webkit-scrollbar': {
+              display: 'none',
+            },
+          }}>
+            {/* Make the list have a max height */}
+            <List>
+              {([...Array(20)].map((_, i) => {
+                const name = `source${i}`;
+
+                const isPressAndRelease = Math.random() > 0.5;
+                const press = `press${i}.mp3`;
+                const release = isPressAndRelease ? `release${i}.mp3` : null;
+
+                return (
+                  <SourceListItem key={i} name={name} press={press} release={release} />
+                )
+              }))}
+            </List>
+          </Box>
+        </Box>
+      </Dialog>
+
+      <Dialog open={addSourceOpen} fullWidth>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          p: 2,
+        }}>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            mb: 2,
+          }}>
+            <Typography variant="h6">Add Source</Typography>
+            <IconButton onClick={() => setAddSourceOpen(false)}>
               <CloseIcon />
             </IconButton>
           </Box>
-          <List>
-            
-          </List>
+          <TextField label="Name" size="small" sx={{ mb: 2 }} />
+          <TextField label="Press Sound" size="small" sx={{ mb: 2 }} />
+          <TextField label="Release Sound" size="small" sx={{ mb: 2 }} />
           <Button
             fullWidth
             variant="contained"
@@ -210,140 +331,151 @@ function Editor() {
         </Box>
       </Dialog>
 
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          ml: 2,
-          mr: 2,
-          mt: 2,
-          pl: 2,
-          pr: 2,
-          pt: 2, 
-          pb: 2,
-        }}
-      >
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}>
-          <Typography variant="h6">Keyboard Sounds Editor</Typography>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            ml: 2,
+            mr: 2,
+            mt: 2,
+            pl: 2,
+            pr: 2,
+            pt: 2, 
+            pb: 2,
+            width: '100%',
+            maxWidth: '1152px',
+          }}
+        >
           <Box sx={{
             display: 'flex',
             flexDirection: 'row',
-            alignItems: 'center',
+            justifyContent: "space-between",
+            alignItems: "center",
           }}>
-            <Tooltip placement="bottom-start" title="View Profile YAML" arrow>
-              <IconButton onClick={() => {}}>
-                <CodeIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip placement="bottom-start" title="Help" arrow>
-              <IconButton onClick={() => {}}>
-                <HelpIcon />
-              </IconButton>
-            </Tooltip>
-            <Divider orientation="vertical" flexItem sx={{ ml: 1, mr: 2 }} />
-            <Button
-              variant="contained"
-              size="small"
-              startIcon={<SaveIcon />}
-            >
-              Save
-            </Button>
+            <Typography variant="h6">Keyboard Sounds Editor</Typography>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+              <Tooltip placement="bottom-start" title="View Profile YAML" arrow>
+                <IconButton onClick={() => {}}>
+                  <CodeIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip placement="bottom-start" title="Help" arrow>
+                <IconButton onClick={() => {}}>
+                  <HelpIcon />
+                </IconButton>
+              </Tooltip>
+              <Divider orientation="vertical" flexItem sx={{ ml: 1, mr: 2 }} />
+              <Button
+                variant="contained"
+                size="small"
+                startIcon={<SaveIcon />}
+              >
+                Save
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Card>
+        </Card>
 
-      <Card
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          ml: 2,
-          mr: 2,
-          mt: 2,
-          pl: 2,
-          pr: 2,
-          pt: 2, 
-          pb: 2,
-          alignItems: 'center',
-        }}
-      >
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          px: 1,
-          mb: 1,
-          width: '100%',
-        }}>
+        <Card
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            ml: 2,
+            mr: 2,
+            mt: 2,
+            pl: 2,
+            pr: 2,
+            pt: 2, 
+            pb: 2,
+            alignItems: 'center',
+            maxWidth: '1152px',
+          }}
+        >
           <Box sx={{
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
+            justifyContent: 'space-between',
+            px: 1,
+            mb: 1,
+            width: '100%',
+            maxWidth: '1120px',
           }}>
-            <Select size="small" sx={{ mr: 1 }} value="1">
-              <MenuItem value="1" selected>Audio Source 1</MenuItem>
-              <MenuItem value="2">Audio Source 2</MenuItem>
-              <MenuItem value="3">Audio Source 3</MenuItem>
-            </Select>
-            <Button variant="outlined" startIcon={<CheckCircleIcon />}>
-              Apply
-            </Button>
-            <Divider orientation="vertical" flexItem sx={{ ml: 2, mr: 2 }} variant="middle" />
-            <Button variant="text" color="warning">
-              Clear Selection
-            </Button>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+              <Select size="small" sx={{ mr: 1 }} value="1">
+                <MenuItem value="1" selected>Audio Source 1</MenuItem>
+                <MenuItem value="2">Audio Source 2</MenuItem>
+                <MenuItem value="3">Audio Source 3</MenuItem>
+              </Select>
+              <Button variant="outlined" startIcon={<CheckCircleIcon />}>
+                Apply
+              </Button>
+              <Divider orientation="vertical" flexItem sx={{ ml: 2, mr: 2 }} variant="middle" />
+              <Button variant="text" color="warning">
+                Clear Selection
+              </Button>
+            </Box>
+            <Box sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+              <Button variant="outlined" sx={{ mr: 1 }} startIcon={<GraphicEqIcon />} color="info" onClick={() => setManageSourcesOpen(true)}>
+                Manage Sources
+              </Button>
+              <Button variant="outlined" startIcon={<SettingsIcon />} color="info" onClick={() => setProfileDetailsOpen(true)}>
+                Profile Details
+              </Button>
+            </Box>
           </Box>
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-          }}>
-            <Button variant="outlined" sx={{ mr: 1 }} startIcon={<GraphicEqIcon />} color="info" onClick={() => setManageSourcesOpen(true)}>
-              Manage Sources
-            </Button>
-            <Button variant="outlined" startIcon={<SettingsIcon />} color="info" onClick={() => setProfileDetailsOpen(true)}>
-              Profile Details
-            </Button>
-          </Box>
-        </Box>
-        <Divider sx={{ mt: 1, mb: 2, ml: 1, mr: 1 }} flexItem />
-        <div className={"keyboardContainer"}>
-          <Keyboard 
-          baseClass={"simple-keyboard-main"}
-            {...keyboardOptions} 
-            theme={"hg-theme-default myTheme1"} 
-          />
-          <div className="controlArrows">
-            <Keyboard
-              baseClass={"simple-keyboard-control"}
-              theme={"hg-theme-default myTheme1"}
-              {...keyboardControlPadOptions}
+          <Divider sx={{ mt: 1, mb: 2, ml: 1, mr: 1 }} flexItem />
+          <div className={"keyboardContainer"}>
+            <Keyboard 
+            baseClass={"simple-keyboard-main"}
+              {...keyboardOptions} 
+              theme={"hg-theme-default myTheme1"} 
             />
-            <Keyboard
-              baseClass={"simple-keyboard-arrows"}
-              theme={"hg-theme-default myTheme1"}
-              {...keyboardArrowsOptions}
-            />
+            <div className="controlArrows">
+              <Keyboard
+                baseClass={"simple-keyboard-control"}
+                theme={"hg-theme-default myTheme1"}
+                {...keyboardControlPadOptions}
+              />
+              <Keyboard
+                baseClass={"simple-keyboard-arrows"}
+                theme={"hg-theme-default myTheme1"}
+                {...keyboardArrowsOptions}
+              />
+            </div>
+            <div className="numPad">
+              <Keyboard
+                baseClass={"simple-keyboard-numpad"}
+                theme={"hg-theme-default myTheme1"}
+                {...keyboardNumPadOptions}
+              />
+              <Keyboard
+                baseClass={"simple-keyboard-numpadEnd"}
+                theme={"hg-theme-default myTheme1"}
+                {...keyboardNumPadEndOptions}
+              />
+            </div>
           </div>
-          <div className="numPad">
-            <Keyboard
-              baseClass={"simple-keyboard-numpad"}
-              theme={"hg-theme-default myTheme1"}
-              {...keyboardNumPadOptions}
-            />
-            <Keyboard
-              baseClass={"simple-keyboard-numpadEnd"}
-              theme={"hg-theme-default myTheme1"}
-              {...keyboardNumPadEndOptions}
-            />
-          </div>
-        </div>
-      </Card>
+        </Card>
+      </Box>
     </ThemeProvider>
   );
 }

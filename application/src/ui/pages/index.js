5322f84aa97d84fc7c8eda30bbea7dc159832745
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2025-05-16 11:37:45 -0500

    Application Version: 1.5.1, Backend Version: 5.9.4

--- application/src/ui/pages/index.js
+++ application/src/ui/pages/index.js
@@ -2,5 +2,6 @@ import { Settings } from "./settings.jsx";
 import { Profiles } from "./profiles.jsx";
 import { Status } from "./status.jsx";
 import { AppRules } from "./app-rules.jsx";
+import { About } from "./about.jsx";
 
-export { Settings, Profiles, Status, AppRules };
+export { Settings, Profiles, Status, AppRules, About };

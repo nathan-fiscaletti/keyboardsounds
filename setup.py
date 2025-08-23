4ba72e43b0cc69434949ce8b62892cf42bf5ba59
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2025-08-23 12:38:58 -0500

    Application Version: 2.0.4, Backend Version: 6.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="keyboardsounds",
-    version="6.0.3",
+    version="6.0.4",
     description="Adds the ability to play sounds while typing on any system.",
     author="Nathan Fiscaletti",
     author_email="nate.fiscaletti@gmail.com",

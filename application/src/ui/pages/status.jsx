17cb9d5b668b9397eae4c08190c39625e4bebb37
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2025-10-02 17:12:28 -0500

    Added UI for configuring pitch shift (non-functional)

--- application/src/ui/pages/status.jsx
+++ application/src/ui/pages/status.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import green from "@mui/material/colors/green";
 
-import { Box, Typography, FormControl, Select, Slider, MenuItem, Tooltip, IconButton, Divider } from "@mui/material";
+import { Box, Typography, FormControl, Select, Slider, MenuItem, Tooltip, IconButton, Divider, Switch, TextField } from "@mui/material";
 
 import VolumeOffIcon from "@mui/icons-material/VolumeOff";
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import KeyboardIcon from '@mui/icons-material/Keyboard';
 import MouseIcon from '@mui/icons-material/Mouse';
 import TipsAndUpdatesIcon from '@mui/icons-material/TipsAndUpdates';
+import InfoIcon from '@mui/icons-material/Info';
 
 const Status = ({
     profilesKeyboardLoaded,
@@ -132,11 +133,12 @@ const Status = ({
           </Select>
         </Box>
       </FormControl>
+
       <Typography
         variant="body1"
         sx={{
-            mt: 3,
             fontWeight: "bold",
+            mt: 2.5,
         }}
       >
         Volume
@@ -180,7 +182,81 @@ const Status = ({
           </IconButton>
         </Tooltip>
       </Box>
+      </Box>
+      <Box
+        sx={{
+          borderRadius: 1,
+          pt: 1.5,
+          pb: 2.5,
+          pr: 3,
+          pl: 3,
+          mt: 2,
+          bgcolor: "#292929",
+        }}
+      >
+      <Box sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+      }}>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}>
+          <Typography
+            variant="body1"
+            sx={{
+                fontWeight: "bold",
+            }}
+          >
+            Pitch Shift
+          </Typography>
+          <Tooltip title="When enabled, the pitch of the audio played for each keypress will be shifted by a random semitone value between your configured upper and lower values." placement="left" arrow>
+            <InfoIcon sx={{ ml: 1, mt: -0.25 }} fontSize="small" />
+          </Tooltip>
+        </Box>
+        <Switch sx={{ mr: -1.5 }} />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          mt: 4,
+        }}
+      >
+        {/* <TextField size="small" type="number" label="Lower ST" />
+        <Divider orientation="vertical" variant="middle" flexItem sx={{ ml: 1, mr: 1 }} />
+        <TextField size="small" type="number" label="Upper ST" /> */}
+        <Slider
+          disableSwap
+          defaultValue={[-2, 2]}
+          min={-12}
+          max={12}
+          size="small"
+          steps={24}
+          valueLabelDisplay="on"
+          valueLabelFormat={(value) => value > -1 ? `+${value}st` : `${value}st`}
+          marks={[{ value: -10, label: '-10st' }, { value: -6, label: '-6st' }, { value: -2, label: '-2st' }, { value: 0, label: '-' }, { value: 2, label: '+2st' }, { value: 6, label: '+6st' }, { value: 10, label: '+10st' }]}
+        />
+      </Box>
+      </Box>
       <Divider sx={{ mt: 2, mb: 1.5 }} />
+      <Box
+        sx={{
+          borderRadius: 1,
+          pt: 2.5,
+          pb: 2.5,
+          pr: 3,
+          pl: 3,
+          mt: 2,
+          bgcolor: "#292929",
+        }}
+      >
       <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
         <TipsAndUpdatesIcon sx={{ color: green[400], mr: 1 }} fontSize="small" />
         <Typography variant="body2" color="text.secondary" sx={{ cursor: 'default' }}>

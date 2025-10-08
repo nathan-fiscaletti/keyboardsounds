763facd7925d88ff519c45e74e88a7f43399466e
Nathan Fiscaletti <nate.fiscaletti@gmail.com>
2025-10-08 10:56:06 -0500

    wip embedded python 3.12 with version fixed and profiles

--- keyboardsounds/root.py
+++ keyboardsounds/root.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import shutil
 
 
 def get_root():
@@ -9,7 +10,17 @@ def get_root():
         else:
             root = os.path.join(os.path.expanduser("~"), ".keyboardsounds")
 
-        os.makedirs(os.path.join(root, "profiles"), exist_ok=True)
+        profiles_dir = os.path.join(root, "profiles")
+        if not os.path.exists(profiles_dir):
+            # Create the profiles directory
+            os.makedirs(profiles_dir, exist_ok=True)
+
+            # Copy existing profiles from application directory
+            application_dir = os.path.dirname(sys.executable)
+            application_profiles_dir = os.path.join(application_dir, "profiles")
+            if os.path.exists(application_profiles_dir):
+                shutil.copytree(application_profiles_dir, profiles_dir)
+
         return root
     else:
         return os.path.dirname(os.path.realpath(__file__))
